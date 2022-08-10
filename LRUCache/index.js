cfeaa5e472e4b6e79cce5df5dcca0694ac103bb3
nc-minh <nguyencongminh.official@gmail.com>
2022-08-10 16:56:07 +0700

    lruCache algorithm

---
+++ LRUCache/index.js
@@ -0,0 +1,37 @@
+class LRUCache {
+	constructor(size) {
+		this.size = size || 10;
+		this.cache = new Map();
+	}
+
+	put(key, value) {
+		const hasKey = this.cache.has(key);
+		if (hasKey) {
+			this.cache.delete(key);
+		}
+
+		this.cache.set(key, value);
+
+		if (this.cache.size > this.size) {
+			this.cache.delete(this.cache.keys().next().value);
+		}
+	}
+
+	get(key) {
+		const hasKey = this.cache.has(key);
+
+		if (hasKey) {
+			const value = this.cache.get(key);
+			this.cache.delete(key);
+			this.cache.set(key, value);
+
+			return value;
+		}
+
+		return -1;
+	}
+
+	getCache() {
+		return this.cache.entries();
+	}
+}
